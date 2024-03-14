53e86fb0a00ccee9fe9a2699dcf2cbb6211f1184
victorfigma@gmail.com <victorfigma@gmail.com>
2024-03-14 19:07:14 +0100

    Add profile section

--- js/themes.js
+++ js/themes.js
@@ -1,37 +1,49 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var icons = document.getElementsByClassName('theme-swap');
-  var isDarkMode = localStorage.getItem('isDarkMode');
+var icons = document.getElementsByClassName('theme-swap');
+var profileImg = document.getElementById('profile-image');
+var iconLkd = document.getElementById('iconLkd');
+var iconGit = document.getElementById('iconGit');
+var iconContact = document.getElementById('iconContact');
+var isDarkMode = localStorage.getItem('isDarkMode');
 
+document.addEventListener("DOMContentLoaded", function() {
   // Load stored theme preference
   if (isDarkMode != null) {
-    if(isDarkMode == "true"){
-      document.body.classList.toggle('light-theme');
-      for (var i = 0; i < icons.length; i++) {
-        icons[i].src = './images/theme-light.svg';
-      }
-    }else{
-      document.body.classList.toggle('dark-theme');
-      for (var i = 0; i < icons.length; i++) {
-        icons[i].src = './images/theme-dark.svg';
-      }
+    if (isDarkMode == 'true') {
+      setDarkTheme();
     }
-  }else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    //Load browser's theme
-    document.body.classList.toggle('dark-theme');
-    for (var i = 0; i < icons.length; i++) {
-      icons[i].src = './images/theme-dark.svg';
-    }
+  } else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    // Load browser's theme
+    setDarkTheme();
   }
 
   // Switch button
   for (var i = 0; i < icons.length; i++) {
     icons[i].onclick = function() {
-        document.body.classList.toggle('dark-theme');
-        isDarkMode = document.body.classList.contains('dark-theme');
-        localStorage.setItem('isDarkMode', !isDarkMode);
-        for (var j = 0; j < icons.length; j++) {
-          icons[j].src = isDarkMode ? './images/theme-dark.svg' : './images/theme-light.svg';
-        }
+        isDarkMode = document.body.classList.contains('dark-theme') ? 'false' : 'true';
+        localStorage.setItem('isDarkMode', isDarkMode);
+        isDarkMode == 'false' ? setLightTheme() : setDarkTheme();
     }
   }
 });
+
+function setLightTheme(){
+  document.body.classList.toggle('dark-theme');
+  for (var j = 0; j < icons.length; j++) {
+    icons[j].src = './images/theme-light.svg';
+  }
+  profileImg.src = './images/victor-light.png';
+  iconLkd.src = './images/linkedin-mark-light.svg';
+  iconGit.src = './images/github-mark-light.svg';
+  iconContact.src = './images/contact-mark-light.png';
+}
+
+function setDarkTheme(){
+  document.body.classList.toggle('dark-theme');
+  for (var j = 0; j < icons.length; j++) {
+    icons[j].src = './images/theme-dark.svg';
+  }
+  profileImg.src = './images/victor-dark.png';
+  iconLkd.src = './images/linkedin-mark-dark.svg';
+  iconGit.src = './images/github-mark-dark.svg';
+  iconContact.src = './images/contact-mark-dark.png';
+}
